9993a6dceb54764f4a4d4d7f86a8cff029a2d0bc
ricohiggs <ricardoahiggs@gmail.com>
2024-11-07 03:36:12 +0000

    added script.js file

---
+++ script.js
@@ -0,0 +1,9 @@
+let favoriteFood =  "tacos"
+console.log(favoriteFood)
+favoriteFood = "wingstop"
+console.log(favoriteFood)
+
+const favoriteHoliday = "thanksgiving"
+console.log(favoriteHoliday)
+
+console.log("value of favoriteFood", favoriteFood)
